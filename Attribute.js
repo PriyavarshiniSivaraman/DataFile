ef64c982a60f11c7f0b2582c711660550c628e46
Priyavarshini S <priyavarshini324@gmail.com>
2023-09-06 14:37:43 +0530

    added

---
+++ Attribute.js
@@ -0,0 +1,29 @@
+ const Part=
+{
+  "msg": "OK",
+  "data": [
+    "V_Name",
+    "Lead Time Duration",
+    "UOM Type",
+    "Change Controlled",
+    "Release Phase",
+    "Design Purchase",
+    "End Item",
+    "End Item Override Enabled",
+    "Spare Part",
+    "Originator",
+    "Part Classification",
+    "Effectivity Date",
+    "Estimated Cost",
+    "Lead Time",
+    "Material Category",
+    "Production Make Buy Code",
+    "Service Make Buy Code",
+    "Target Cost",
+    "Unit of Measure",
+    "Weight",
+    "Current Version",
+    "Is Version",
+    "isVPMVisible"
+  ]
+}
