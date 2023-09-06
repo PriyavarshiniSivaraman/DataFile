219bea1ad80c1835f45d2ddf962174fc2167bf77
Priyavarshini S <priyavarshini324@gmail.com>
2023-09-06 18:12:32 +0530

    ok

---
+++ Type.js
@@ -0,0 +1,174 @@
+ const Type=
+ {
+  "msg": "OK",
+  "data": [
+    {
+      "type": "VPLMDataMigration"
+    },
+    {
+      "type": "VPLM Project Template"
+    },
+    {
+      "type": "VPLMCounter"
+    },
+    {
+      "type": "PLMCfgVariabilitySpace"
+    },
+    {
+      "type": "PLMPosProjectResourceMngtRef"
+    },
+    {
+      "type": "PLMPosSettingsEnvironmentRef"
+    },
+    {
+      "type": "VPLMInteg-SynchParameters"
+    },
+    {
+      "type": "ENOWSP_WorkpaceRootRef"
+    },
+    {
+      "type": "eService Trigger Program Parameters"
+    },
+    {
+      "type": "eService Object Generator"
+    },
+    {
+      "type": "eService Number Generator"
+    },
+    {
+      "type": "Notification"
+    },
+    {
+      "type": "Financial Benefit Category"
+    },
+    {
+      "type": "Financial Cost Category"
+    },
+    {
+      "type": "Person"
+    },
+    {
+      "type": "Plant"
+    },
+    {
+      "type": "IZCourse"
+    },
+    {
+      "type": "CAD Model"
+    },
+    {
+      "type": "AppDefinition"
+    },
+    {
+      "type": "Route"
+    },
+    {
+      "type": "Inbox Task"
+    },
+    {
+      "type": "Change Request"
+    },
+    {
+      "type": "IZIBuyerDesk"
+    },
+    {
+      "type": "IZVehicles"
+    },
+    {
+      "type": "Change Action"
+    },
+    {
+      "type": "Proposed Activity"
+    },
+    {
+      "type": "Change Order"
+    },
+    {
+      "type": "Part"
+    },
+    {
+      "type": "Part Master"
+    },
+    {
+      "type": "Impact Analysis"
+    },
+    {
+      "type": "Named Effectivity"
+    },
+    {
+      "type": "Task"
+    },
+    {
+      "type": "Personal Workspace"
+    },
+    {
+      "type": "Business Role"
+    },
+    {
+      "type": "Realized Activity"
+    },
+    {
+      "type": "Route Template"
+    },
+    {
+      "type": "Push"
+    },
+    {
+      "type": "Security Context"
+    },
+    {
+      "type": "IZStudent"
+    },
+    {
+      "type": "Gate"
+    },
+    {
+      "type": "CEnv Service"
+    },
+    {
+      "type": "Company"
+    },
+    {
+      "type": "PnOProject"
+    },
+    {
+      "type": "IZIssue"
+    },
+    {
+      "type": "Affinity"
+    },
+    {
+      "type": "Document"
+    },
+    {
+      "type": "Project Access List"
+    },
+    {
+      "type": "Project Template"
+    },
+    {
+      "type": "Route Task User"
+    },
+    {
+      "type": "Milestone"
+    },
+    {
+      "type": "Phase"
+    },
+    {
+      "type": "Computing Environment"
+    },
+    {
+      "type": "OntoOntology"
+    },
+    {
+      "type": "OntoLiteral"
+    },
+    {
+      "type": "OntoNamedConcept"
+    },
+    {
+      "type": "OntoDataProperty"
+    }
+  ]
+}
